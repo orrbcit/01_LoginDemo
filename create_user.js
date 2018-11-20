609575ac13e5dbb9c6f206a344bffd072fcc68dc
Carly Orr <carly_wong_orr@bcit.ca>
2018-11-20 15:06:13 -0800

    Update create_user.js
    add comments

--- create_user.js
+++ create_user.js
@@ -2,13 +2,14 @@
 
     var firebase = app_firebase;
 
-    // for the current user
-    // create this user node in the datebase
+    // for the currently authenticated firebase user
+    // create this your own "user" node in the datebase using 
+    // the firebase generated "uid"
 
     firebase.auth().onAuthStateChanged(function(user){
         firebase.database().ref("users/"+user.uid).update(
-		{
-        "name":user.displayName, 
+	{
+         "name":user.displayName, 
          "email":user.email
         });
     });
